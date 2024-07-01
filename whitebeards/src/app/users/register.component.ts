fe2b0af38c8a0de80eef2727bb59cf0fc0dc44c7
elieljohn <ejmalabar88@gmail.com>
2024-07-01 18:26:24 +0100

    Component Member Sequence
    1. Create a private saveAndRedirect() method, then modify registerUser so that it calls saveAndRedirect()
    register.component.ts: line 36-39 & 45-51
    
    Component Member Sequence
    1. Properties
    2. Constructor
    3. Public Functions
    4. Private Functions

--- whitebeards/src/app/users/register.component.ts
+++ whitebeards/src/app/users/register.component.ts
@@ -35,14 +35,18 @@ export class RegisterComponent {
 
   registerUser(user: IUser) {
     this.saving = true;
+    this.saveAndRedirect(user);
+  }
+
+  cancel() {
+    this.router.navigate(['/']);
+  }
+
+  private saveAndRedirect(user: IUser) {
     this.userRepository.saveUser(user)
       .subscribe({
         error: () => this.saving = false,
         complete: () => this.router.navigate(['/catalog'])
       });
-  }
-
-  cancel() {
-    this.router.navigate(['/']);
   }
 }
