089c4a5935dc40ccc823f49d311004796f9c474f
elieljohn <ejmalabar88@gmail.com>
2024-07-01 11:55:11 +0100

    One Item per File
    1. Build register.component.ts and move RegisterComponent there, then fix imports accordingly

--- whitebeards/src/app/app.module.ts
+++ whitebeards/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar.component';
 import { CatalogComponent } from "./catalog/catalog.component";
-import { RegisterComponent } from "./users/sign-in.component";
+import { RegisterComponent } from "./users/register.component";
 import { SignInComponent } from "./users/sign-in.component";
 import { LoadingComponent } from "./components/loading-spinner.component";
 import { DataRepositoryService } from "./services/data-repository.service"
